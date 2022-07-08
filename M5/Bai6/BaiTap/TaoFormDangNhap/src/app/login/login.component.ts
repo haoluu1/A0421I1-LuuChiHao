74516e4b8722c6167e17ddb4b0e8a3a3ea20c2c8
HaoLuu <luuchihao999@gmail.com>
2022-07-08 16:02:01 +0700

    BaiTap

---
+++ M5/Bai6/BaiTap/TaoFormDangNhap/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from "../user";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  users :User[]=[{email:'tuan@gmail.com',password:'123456'},{email:'admin@gmail.com',password:'123456'},{email:'member@gmail.com',password:'123456'}];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  formLogin = new FormGroup({
+      email: new FormControl('',[Validators.required,Validators.pattern("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")]),
+      password: new FormControl('',[Validators.required,Validators.minLength(6)])
+    }
+  )
+
+  onSubmit() {
+    console.log(this.formLogin);
+  }
+}
